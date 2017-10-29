d2bd5d872434a55521435f1210d366d4970b2aa7
Seregey Pavlenko <pavlenkosrga@gmail.com>
2017-10-29 21:09:27 +0800

    Stop project commit

--- resources/assets/build/public-path.js
+++ resources/assets/build/public-path.js
@@ -2,6 +2,7 @@
 /* globals SAGE_DIST_PATH */
 
 /** Dynamically set absolute public path from current protocol and host */
+SAGE_DIST_PATH='/wp-content/themes/sage/autos/dist'
 if (SAGE_DIST_PATH) {
   __webpack_public_path__ = SAGE_DIST_PATH; // eslint-disable-line no-undef, camelcase
 }
